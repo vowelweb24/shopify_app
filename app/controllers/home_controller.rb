81af9859a3e92701c21b9076801e8ca0133e9f3f
Ruturaj Kalal <ruturaj@hazeltons.ca>
2017-12-22 09:55:42 -0500

    ami

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10, page: 1 })
-    @orders = ShopifyAPI::Order.find(:all)
+    # @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 5, page:1})
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
