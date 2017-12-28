27b63f257f70d945b2f4c7ad703430bd24f20d27
Ruturaj Kalal <ruturaj@hazeltons.ca>
2017-12-27 21:53:40 -0500

    update redirect uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,6 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = "141873b9ad4d349242c652f3557abf19"
   config.secret = "00653a682017a87ba36f156e4894ec74"
   config.scope = "read_orders, read_products"
+  config.redirect_uri ="http://test-project-app.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
