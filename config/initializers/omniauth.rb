fc29511e04920411682877ba037dbe73c237e1e3
Ruturaj Kalal <ruturaj@hazeltons.ca>
2017-12-27 22:39:24 -0500

    update redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
            ShopifyApp.configuration.api_key,
            ShopifyApp.configuration.secret,
-           :redirect_uri => ShopifyApp.configuration.redirect_uri,
            scope: ShopifyApp.configuration.scope,
            setup: lambda { |env|
              strategy = env['omniauth.strategy']
