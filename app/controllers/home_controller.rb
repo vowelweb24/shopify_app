989f05141c1dc6cbd5b51632649244f08fc59f6e
Ruturaj Kalal <ruturaj@hazeltons.ca>
2017-12-28 20:31:24 -0500

    table for fraud check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,20 @@ class HomeController < ShopifyApp::AuthenticatedController
     @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :status => "any" })
     @order_count = ShopifyAPI::Order.count
     
+
+@page = 1 
+while @order_count > 0 do 
+  @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :status => "any",:page=> @page })
+  # perform work on products
+
+  @order_count = @order_count - 250
+  @page = @page + 1
+end
+
     @webhooks = ShopifyAPI::Webhook.find(:all)
 
 
 
-    @uri = URI.parse("https://#{@shop_session.url}/admin/orders.json")
+    # @uri = URI.parse("https://#{@shop_session.url}/admin/orders.json")
   end
 end
