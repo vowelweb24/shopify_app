0f16df5037f583ccf8e83020f0d9183741ba60bb
vowelweb24 <35296401+vowelweb24@users.noreply.github.com>
2018-01-17 15:26:49 +0530

    extended order limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,10 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { page: 100 }) 
+  
     @orders = ShopifyAPI::Order.find(:all, :params => {:status => "any" })
     @order_count = ShopifyAPI::Order.count
     
-    @page = 1 
-    while @order_count > 0 do 
-      @orders = ShopifyAPI::Order.find(:all, :params => {:status => "any",:page=> @page })
-       # perform work on products
-
-      @order_count = @order_count - 500000
-      @page = @page + 1
-end
-
-    @webhooks = ShopifyAPI::Webhook.find(:all)
+    
 
 
 
